6147929d9b88ba0c87dab207157cf24241c33eac
raghav-b <raghavbhardwaj@u.nus.edu>
2019-08-29 15:27:44 +0800

    Finished level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
 public class Duke {
+    private static String logo = "         ____        _        \n"
+        + "        |  _ \\ _   _| | _____ \n"
+        + "        | | | | | | | |/ / _ \\\n"
+        + "        | |_| | |_| |   <  __/\n"
+        + "        |____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        InputHandler inputReq = new InputHandler();
+        System.out.println(logo);
+        printOutput("Greetings, I am Duke!\n        How may I be of assistance?");
+
+        while (true) {
+            String curInput = inputReq.getInput();
+
+            if (curInput.toLowerCase() == "bye") {
+                quitFunc();
+                break;
+            }
+
+            printOutput(curInput);
+        }
+    }
+
+    private static void printOutput(String stringToOutput) {
+        System.out.println("        ____________________________________________________________");
+        System.out.println("        " + stringToOutput);
+        System.out.println("        ____________________________________________________________\n");
+    }
+
+    private static void quitFunc() {
+    }
+
+    private static void quitFunc(String optionalOutput) {
+        printOutput(optionalOutput);
+        printOutput("Farewell. I do hope to see you again.");
     }
 }
---
+++ src/main/java/InputHandler.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InputHandler {
+    public static String getInput() {
+        Scanner myScanner = new Scanner(System.in);
+        String input = myScanner.nextLine();
+
+        return input;
+    }
+}
