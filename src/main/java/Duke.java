f5e3d8fd7da18e4bee5c0f944482ced603c4e1e4
raghav-b <raghavbhardwaj@u.nus.edu>
2019-08-29 16:14:02 +0800

    Added task handler class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,66 @@
 public class Duke {
-    private static String logo = "         ____        _        \n"
-        + "        |  _ \\ _   _| | _____ \n"
-        + "        | | | | | | | |/ / _ \\\n"
-        + "        | |_| | |_| |   <  __/\n"
-        + "        |____/ \\__,_|_|\\_\\___|\n";
+    private static String logo = addIndent() + "____        _        \n"
+        + addIndent() + "|  _ \\ _   _| | _____ \n"
+        + addIndent() + "| | | | | | | |/ / _ \\\n"
+        + addIndent() + "| |_| | |_| |   <  __/\n"
+        + addIndent() + "|____/ \\__,_|_|\\_\\___|\n";
 
     public static void main(String[] args) {
+        System.out.print(logo + "\n");
+        printOutput(addIndent() + "Greetings, I am Duke!\n" + addIndent()
+                + "How may I be of assistance?\n");
+
         InputHandler inputReq = new InputHandler();
-        System.out.println(logo);
-        printOutput("Greetings, I am Duke!\n        How may I be of assistance?");
+        TaskHandler curList = new TaskHandler();
 
         while (true) {
             String curInput = inputReq.getInput();
+            //System.out.println(curInput);
+            //System.out.println(curInput.toLowerCase());
+            //System.out.println(curInput);
 
-            if (curInput.toLowerCase() == "bye") {
+            if (curInput.toLowerCase().equals("bye")) {
                 quitFunc();
                 break;
             }
 
-            printOutput(curInput);
+            else if (curInput.toLowerCase().equals("list")) {
+                if (curList.getListLength() == 0) {
+                    printOutput(addIndent() + "Your task list is currently empty.\n");
+                } else {
+                    String tempString = addIndent() + "Now listing items in your task list:\n";
+                    for (int i = 1; i < curList.getListLength() + 1; i++) {
+                        tempString += (addIndent() + i + ". " + curList.getListItem(i - 1) + "\n");
+                    }
+                    printOutput(tempString);
+                }
+            }
+
+            else if (curInput.toLowerCase().equals("clear")) {
+                int tempListLength = curList.getListLength()
+                curList.clearList();
+                printOutput(addIndent() + "I have cleared your task list of " +
+                         tempListLength + " items.\n");
+            }
+
+            else {
+                curList.addListItem(curInput);
+                printOutput("I have added: '" + curInput + "'.\n");
+            }
         }
     }
 
     private static void printOutput(String stringToOutput) {
-        System.out.println("        ____________________________________________________________");
-        System.out.println("        " + stringToOutput);
-        System.out.println("        ____________________________________________________________\n");
+        System.out.print(addIndent() + "____________________________________________________________\n");
+        System.out.print(stringToOutput);
+        System.out.print(addIndent() + "____________________________________________________________\n\n");
+    }
+    private static String addIndent() {
+        return "        ";
     }
 
     private static void quitFunc() {
+        printOutput(addIndent() + "Farewell. I do hope to see you again.\n");
     }
 
     private static void quitFunc(String optionalOutput) {
--- src/main/java/TaskHandler.java
+++ src/main/java/TaskHandler.java
@@ -1,4 +1,25 @@
+import java.util.Vector;
+
 public class TaskHandler {
+    private Vector<String> taskList;
 
+    TaskHandler() {
+        taskList = new Vector();
+    }
 
+    public void addListItem(String itemToAdd) {
+        taskList.add(itemToAdd);
+    }
+
+    public String getListItem(int itemIndex) {
+        return taskList.get(itemIndex);
+    }
+
+    public int getListLength() {
+        return taskList.size();
+    }
+
+    public void clearList() {
+        taskList.clear();
+    }
 }
